3115dde58be63c6ee7a49dbd42e2f1ee5eb2bce5
Chris Agold <christophersagold@gmail.com>
2021-03-12 10:07:44 -0600

    added albums example

--- src/main/java/AdsIndexServlet.java
+++ src/main/java/AdsIndexServlet.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-
 @WebServlet(name = "AdsIndexServlet", urlPatterns = "/ads")
 public class AdsIndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
